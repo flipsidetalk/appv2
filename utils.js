ac62edba2c0efd7ed2266a24cf30d070004a979e
forrestsill <forrestsill@gmail.com>
2017-08-30 20:41:29 -0700

    External request changes

--- utils.js
+++ utils.js
@@ -14,13 +14,13 @@ module.exports.upsert = function(table, values, condition) {
 module.exports.makeExternalRequest = function(request, url, data, success) {
   request({
       url: url,
-      method: 'GET',
+      method: 'POST',
       json: true,
       body: data
   }, function (error, response, body) {
-    console.log('HERE: ' + JSON.stringify(response));
+    console.log('Response: ' + response);
     if (!error) {
-      success(response);
+      success(body);
     }
   });
 }
