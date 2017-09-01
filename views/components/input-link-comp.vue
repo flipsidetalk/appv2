0b76cd35d854975df25de71b958ce781cb0ea8cb
forrestsill <forrestsill@gmail.com>
2017-09-01 00:55:26 -0700

    Copied homepage from nodeapp

---
+++ views/components/input-link-comp.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row section section-first-text">
+    <div class="col-md-offset-1 col-md-8">
+      <div class="bumpDown">
+        <h1>Flipside</h1>
+        <h2 class="u-lighter">Share stories for better discourse</h2>
+      </div>
+      <form class="submitForm center-heading bumpDown" action="index.html" method="post">
+        <label for="" class="submitButton">URL:</label>
+        <input class="mainInput" type="text" name="" value="" placeholder="https://www.nytimes.com/opinion">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['introcomp']
+}
+</script>
+
+<style lang="css">
+
+</style>
