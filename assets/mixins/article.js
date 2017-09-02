905b41e709ad168f3984262565effd7ae6584a9c
forrestsill <forrestsill@gmail.com>
2017-09-01 22:13:56 -0700

    Comment submission code

--- assets/mixins/article.js
+++ assets/mixins/article.js
@@ -4,7 +4,7 @@ var mixin = {
       textcomp.whyResponse.sentenceId = textcomp.lastReferenced;
       textcomp.whyResponse.vote = textcomp.article.sentences[textcomp.lastReferenced].seen;
       textcomp.whyResponses.push(textcomp.whyResponse)
-      //this.postWhy(textcomp.whyResponse.input, textcomp.whyResponse.sentenceId);
+      this.postResponse(textcomp.whyResponse.input, textcomp.whyResponse.sentenceId);
       textcomp.whyResponse = {
         sentenceId: "",
         input: "",
@@ -13,6 +13,23 @@ var mixin = {
       textcomp.responseSubmitted = 1;
       textcomp.responseForm = 0;
       textcomp.talkdisplay = "none";
+    },
+    postResponse: function(statement, sentenceId) {
+      var data = {
+        sentenceId: sentenceId,
+        statement: statement
+      }
+      $.ajax({
+        type: 'POST',
+        url: '/submitResponse',
+        data: data,
+        success: function() {
+          console.log("sendsuccess: " + data);
+        },
+        error: function() {
+          console.log("error: " + data);
+        }
+      });
     }
   }
 };
