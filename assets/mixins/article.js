8a72cc132b7fa12d109ed452b4899a8d0be16485
forrestsill <forrestsill@gmail.com>
2017-09-11 13:51:53 -0700

    Moved submitWhy and postResponse from textComp.js to article.js

--- assets/mixins/article.js
+++ assets/mixins/article.js
@@ -122,6 +122,34 @@ var mixin = {
         $('#'+bubbleId).attr("fill", bubbleFill);
       }
 
+    },
+    submitWhy: function(textcomp) {
+      textcomp.whyResponse.sentenceId = textcomp.lastReferenced;
+      textcomp.whyResponse.vote = textcomp.article.sentences[textcomp.lastReferenced].seen;
+      textcomp.whyResponses.push(textcomp.whyResponse)
+      this.postResponse(textcomp.whyResponse.input, textcomp.whyResponse.sentenceId);
+      textcomp.whyResponse = {
+        sentenceId: "",
+        input: "",
+        vote: ""
+      };
+    },
+    postResponse: function(statement, sentenceId) {
+      var data = {
+        sentenceId: sentenceId,
+        statement: statement
+      }
+      $.ajax({
+        type: 'POST',
+        url: '/submitResponse',
+        data: data,
+        success: function() {
+          console.log("sendsuccess: " + data);
+        },
+        error: function() {
+          console.log("error: " + data);
+        }
+      });
     }
   },
   mounted: function() {
